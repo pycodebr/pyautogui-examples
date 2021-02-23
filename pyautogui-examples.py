1d7428b32023f9fda8e1703d1ba23cf32a45dbe9
pycodebr <pycodebr@gmail.com>
2021-02-23 18:03:58 -0300

    first commit

---
+++ pyautogui-examples.py
@@ -0,0 +1,9 @@
+import pyautogui
+
+pyautogui.alert('Alerta PyAutoGUI!')
+
+pyautogui.confirm('Tem certeza que deseja fazer isso?')
+
+pyautogui.prompt('Digite seu nome')
+
+pyautogui.password('Digite sua senha')
